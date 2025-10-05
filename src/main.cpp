538f557a3a7ebecec87a78593e3b960cc796c1dd
shiba-25 <chestnut9165@gmail.com>
2025-10-05 16:14:49 +0900

    最終変更

--- src/main.cpp
+++ src/main.cpp
@@ -2,45 +2,56 @@
 #define Radian M_PI/180
 BufferedSerial esp(PA_9,PA_10,115200);
 BufferedSerial pc(USBTX,USBRX,115200);
-int16_t pwm[4] {0};
+char buf[16];
+char *token;
+int JoystickX,JoystickY,power;
+int wheel[4] = {0,0,0,0};
+int wheel_angle[4] = {45,135,225,315};
+const int motor_max_val = 15000;
+int turn;
 
 int main(){
-    CAN can(PB_12, PB_13, (int)1e6); // canを出力するピンを指定
-    CANMessage msg;// 変数「msg」の作成
-
-    char buf[16];
-    char *token;
-    int JoystickX,JoystickY,power;
-    int wheel[4] = {0,0,0,0};
-    int wheel_dig[4] = {45,135,225,315};
+    CAN can(PA_11, PA_12, (int)1e6); //canを出力するピンを指定
+    CANMessage msg; //変数「msg」の作成
+    
     while(1){
-        if(pc.readable()){
-            int n = pc.read(buf, sizeof(buf) - 1);
+        if(esp.readable()){
+            int n = esp.read(buf, sizeof(buf) - 1);
             if(n > 0){
                 buf[n] = '\0';
-                if(buf[n-1] == '|'){
-                    buf[n-1] = '\0';
+                if(buf[n-1] == '|') buf[n-1] = '\0';
+                if(buf == "R"){
+                    turn = 1;
+                } else if(buf == "L"){
+                    turn = -1;
+                } else{
+                    turn = 0;
                 }
                 if(buf != "R",buf != "L"){
-                    char *str = buf;
-                    token = strtok(str,",");
-                    JoystickX = atoi(token);
-                    token = strtok(NULL,",");
-                    JoystickY = atoi(token);
+                    char *str = buf; //bufの中身をstrに保存
+                    token = strtok(str,","); //","で区切る
+                    JoystickX = atoi(token); //文字列を数値に変換
+                    token = strtok(NULL,","); //","で区切る
+                    JoystickY = atoi(token); //文字列を数値に変換
                 }
             }
         }
+        // ジョイスティックの倒した強さと角度を計算
         power = sqrt(pow(JoystickX,2.0)+pow(JoystickY,2.0));
         float Joystickdir = atan2(JoystickX,JoystickY);
-        if(Joystickdir < 0){
-            Joystickdir += 360.0f * Radian;
+        if(Joystickdir < 0) Joystickdir += 360.0f * Radian;
+        // オムニのそれぞれの比率を計算、または旋回
+        if(turn != 0){
+            for(int i = 0; i < 4; i++){
+                wheel[i] = motor_max_val*turn;
+            }
+        }else {
+            for(int i = 0; i < 4; i++){
+                wheel[i] = min((int)sin(Joystickdir-wheel_angle[i]*Radian),motor_max_val);
+            }
         }
-        for(int i = 0; i < 4; i++){
-            wheel[i] = sin(Joystickdir-wheel_dig[i]*Radian);
-            pwm[i] =  wheel[i]*power*100;
-        }
-        printf("%d,%d",JoystickX,JoystickY);
-        CANMessage msg(2, (const uint8_t *)pwm, 8); //特に理由がない限りwhile直下
+        
+        CANMessage msg(2, (const uint8_t *)wheel, 8); //特に理由がない限りwhile直下
         can.write(msg); //特に理由がない限りwhile直下
     }
 }
